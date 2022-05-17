6532269f72a5c0e0685bf6fe799b55c81a88b063
hamanaikumi <ikumi.hamana@rakus-partners.co.jp>
2022-05-17 14:57:31 +0900

    Style MemoList Screen

--- App.jsx
+++ App.jsx
@@ -1,29 +1,126 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
-import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* true の省略記法 */}
-      <Hello bang>World</Hello>
-      <Hello bang style={{ fontSize: 16 }}>
-        World
-      </Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>Memo App</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年5月17日 10::00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年5月17日 10::00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2022年5月17日 10::00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // flex 1:画面いっぱいに表示する
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#F0F4F8",
+    // 横中央揃え
+    // alignItems: "center",
+    // 縦中央揃え
+    // justifyContent: "center",
+  },
+  appbar: {
+    width: "100%",
+    height: 104,
+    backgroundColor: "#467FD3",
+    // 配置
+    justifyContent: "flex-end",
+  },
+  appbarInner: {
     alignItems: "center",
+  },
+  appbarRight: {
+    position: "absolute",
+    right: 19,
+    bottom: 16,
+    color: "rgba(255,255,255,0.8)",
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+  memoListItem: {
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0,0,0,0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
+  },
+  circleButton: {
+    backgroundColor: "#467FD3",
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    // shadowプロパティはiOSのみ
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    // elevationはAndroidのみ対応
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
