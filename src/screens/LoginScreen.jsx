2d2090f74775dcdb7409232cec4d3ddc0789fe2b
hamanaikumi <ikumi.hamana@rakus-partners.co.jp>
2022-05-18 15:19:38 +0900

    Set TextInput options

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -11,12 +11,36 @@ import Button from "../components/Button";
 
 export default function LoginScreen(props) {
   const { navigation } = props;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
-        <TextInput style={styles.input} value="email address" />
-        <TextInput style={styles.input} value="password" />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+          placeholder="Email Address"
+          // 頭文字を大文字にしない
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          placeholder="Password"
+          // パスワードを伏せ字に
+          secureTextEntry
+          textContentType="password"
+        />
 
         <Button
           label="Submit"
